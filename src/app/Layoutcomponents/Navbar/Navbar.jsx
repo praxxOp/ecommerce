1d04b88454e1123d105852df76ac4e913ec3f4df
prathamesh <shelke.prathamesh0012@gmail.com>
2023-10-13 16:06:16 +0530

    modified:   src/app/Layoutcomponents/Navbar/Navbar.jsx 	modified:   src/app/Layoutcomponents/Navbar/Navbar.module.css

--- src/app/Layoutcomponents/Navbar/Navbar.jsx
+++ src/app/Layoutcomponents/Navbar/Navbar.jsx
@@ -4,7 +4,10 @@ import Menu from "./Navbar.module.css";
 const Navbar = () => {
   return (
     <div className={Menu.main}>
-      <h1>hi</h1>
+      <h1>KICKUP.</h1>
+      <h1>HOME</h1>
+      <h1>MARKETPLACE</h1>
+      <h1>CONTACT</h1>
     </div>
   );
 };
