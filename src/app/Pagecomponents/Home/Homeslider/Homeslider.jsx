a0b7d170d46361823e1d2c37994b44600a945279
prathamesh <shelke.prathamesh0012@gmail.com>
2024-01-02 16:47:31 +0530

    modified:   src/app/Pagecomponents/Home/Homeslider/Homeslider.css 	modified:   src/app/Pagecomponents/Home/Homeslider/Homeslider.jsx 	modified:   src/app/data.js 	renamed:    src/app/ecommerce assets/7b12dffc-1c76-4076-9b6f-187d8c530951-removebg-preview.png -> src/app/ecommerce-assets/7b12dffc-1c76-4076-9b6f-187d8c530951-removebg-preview.png 	renamed:    src/app/ecommerce assets/PUMA-x-HARRDY-SANDHU-Rider-FV-Unisex-Sneakers-removebg-preview.png -> src/app/ecommerce-assets/PUMA-x-HARRDY-SANDHU-Rider-FV-Unisex-Sneakers-removebg-preview.png 	renamed:    src/app/ecommerce assets/RS-X-Brand-Love-Unisex-Sneakers-removebg-preview.png -> src/app/ecommerce-assets/RS-X-Brand-Love-Unisex-Sneakers-removebg-preview.png 	renamed:    src/app/ecommerce assets/RS-X-Efekt-Energy-Unisex-Sneakers-removebg-preview.png -> src/app/ecommerce-assets/RS-X-Efekt-Energy-Unisex-Sneakers-removebg-preview.png 	renamed:    src/app/ecommerce assets/Rider-Future-Vintage-Unisex-Sneakers-removebg-preview.png -> src/app/ecommerce-assets/Rider-Future-Vintage-Unisex-Sneakers-removebg-preview.png 	renamed:    src/app/ecommerce assets/f9ebb7c4-7844-4b40-bd4c-ad2aeb61825b-removebg-preview.png -> src/app/ecommerce-assets/f9ebb7c4-7844-4b40-bd4c-ad2aeb61825b-removebg-preview.png 	renamed:    src/app/ecommerce assets/shoe2-removebg-preview.png -> src/app/ecommerce-assets/first.png 	renamed:    src/app/ecommerce assets/shoe1-removebg-preview 1.png -> src/app/ecommerce-assets/shoe1-removebg-preview 1.png 	renamed:    src/app/ecommerce assets/shoe3-removebg-preview.png -> src/app/ecommerce-assets/shoe3-removebg-preview.png

--- src/app/Pagecomponents/Home/Homeslider/Homeslider.jsx
+++ src/app/Pagecomponents/Home/Homeslider/Homeslider.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import "./Homeslider.css"
-
+import React from 'react';
+import "./Homeslider.css";
+import data from '@/app/data';
+import Nike1 from "../../../ecommerce-assets/first.png";
+import Image from 'next/image';
 const Homeslider = () => {
   return (
-    <div>Homeslider</div>
-  )
-}
+    <div className="main__slider">
+      <p>“Your premier source for fresh, brand new footwear. Explore the latest trends, step out in style, and make every stride a statement. Discover a world of quality, comfort, and fashion at KickUp. Elevate your look today."</p>
+      <div className="imagecontainer">
+        <Image src={Nike1}
+        height={100}
+        width={100}/>
+        <h1>{data[0].Name}</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Homeslider
+export default Homeslider;
--- src/app/data.js
+++ src/app/data.js
@@ -1,47 +1,38 @@
 const data = [
   {
     Name: "Air Flight Huarache.",
-    ImgUrl: "url",
     price: 220,
   },
   {
     Name: "Air Jordan 1 Elevate High SE",
-    ImgUrl: "url",
     price: 220,
   },
   {
     Name: "Rider Future Vintage",
-    ImgUrl: "url",
     price: 220,
   },
   {
     Name: "praxx",
-    ImgUrl: "url",
     price: 220,
   },
   {
     Name: "praxx",
-    ImgUrl: "url",
     price: 220,
   },
   {
     Name: "praxx",
-    ImgUrl: "url",
     price: 220,
   },
   {
     Name: "praxx",
-    ImgUrl: "url",
     price: 220,
   },
   {
     Name: "praxx",
-    ImgUrl: "url",
     price: 220,
   },
   {
     Name: "praxx",
-    ImgUrl: "url",
     price: 220,
   },
 ];
